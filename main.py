86b446bbb47347f8023109c1f27eeb28a316e7df
levsha_spirit <1evsha3243@gmail.com>
2026-07-31 14:42:05 +0400

    parser

--- main.py
+++ main.py
@@ -3,8 +3,31 @@ import asyncio
 from playwright.async_api import async_playwright
 
 
-async def press_button(page, button_name: str):
-    await page.locator(button_name).click()
+# async def main():
+#     async with async_playwright() as p:
+#         browser = await p.chromium.launch(headless=False)
+#         page = await browser.new_page()
+#         await page.goto("https://rostender.info/")
+# search = page.get_by_placeholder(
+#     "Введите ключевые слова, например “ремонт дорог в Новосибирске”"
+# )
+# await search.fill("Видеосъемка")
+# await search.press("Enter")
+# await page.wait_for_load_state("networkidle")
+# await page.locator("input[name='exceptions']").fill("поставка, оборудование")
+# print(page.url)
+# await page.wait_for_timeout(10000)
+# await page.keyboard.press("Tab")
+# await page.locator("#start-search-button").click()
+# print(page.url)
+# await page.wait_for_timeout(20000)
+# html = await page.content()
+# with open("page.html", "w") as f:
+#     f.write(html)
+# print(page.url)
+# await browser.close()
+async def press_button(page):
+    await page.locator("#start-search-button").click()
 
 
 async def fill_and_search(page, input_name: str, fill_str: str):
@@ -21,16 +44,14 @@ async def get_html(page, name_file: str):
         f.write(html)
 
 
-async def main(
-    url: str, input_name: str, fill_str: str, filename: str, button_name: str
-):
+async def main(url: str, input_name: str, fill_str: str, filename: str):
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=False)
         page = await browser.new_page()
         await page.goto(url)
         await fill_and_search(page, input_name, fill_str)
-        await press_button(page, button_name)
-        await page.wait_for_timeout(10000)
+        await press_button(page)
+        await page.wait_for_timeout(1000)
         await get_html(page, filename)
 
 
---
+++ utils.py
@@ -0,0 +1,15 @@
+async def press_button(page, button_name: str):
+    await page.locator(button_name).click()
+
+
+async def fill_and_search(page, input_name: str, fill_str: str):
+
+    await page.locator(input_name).first.fill(fill_str)
+    await page.keyboard.press("Enter")
+    await page.wait_for_timeout(1000)
+
+
+async def get_html(page, name_file: str):
+    html = await page.content()
+    with open(f"{name_file}.html", "w") as f:
+        f.write(html)
